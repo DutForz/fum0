a69e4f40f8ae4d8b452a1a1234beef2312f86849
DutForz <149707392+DutForz@users.noreply.github.com>
2026-05-13 03:34:32 +0300

    full works Builder + Controller chat withOut UI

--- lib/pages/chat/chat_Screan.dart
+++ lib/pages/chat/chat_Screan.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fumo/components/MyTextField.dart';
 import 'package:fumo/core/auth/auth_service.dart';
 import 'package:fumo/core/chat/chat_service.dart';
+import 'package:fumo/extensions/context_extensions.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class chat_Screan extends StatelessWidget {
   final String receiverEmai, recieverID;
@@ -25,9 +28,55 @@ class chat_Screan extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(receiverEmai)),
-      body: Column(children: [Expanded(child: _buildMessageList())]),
+      body: Column(
+        children: [
+          Expanded(child: _buildMessageList()),
+          _buildUserInput(),
+        ],
+      ),
     );
   }
 
-  Widget _buildMessageList() {}
+  Widget _buildMessageList() {
+    String senderId = _authService.getCurrentUSer()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessages(recieverID, senderId),
+      builder: ((context, snapshot) {
+        if (snapshot.hasError) {
+          return Text(context.localizations.error);
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text(context.localizations.loading);
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map((doc) => _buildMessageItem(doc))
+              .toList(),
+        );
+      }),
+    );
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Text(data["message"]);
+  }
+
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: MyTextField(
+            hintText: "...",
+            Obscure: false,
+            controller: _messageController,
+          ),
+        ),
+        IconButton(
+          onPressed: sendMessage,
+          icon: const Icon(Icons.arrow_upward),
+        ),
+      ],
+    );
+  }
 }
